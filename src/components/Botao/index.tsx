9dba3b99a5f968a769a69bd5b500fb0cbd177622
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-09 18:53:40 -0300

    :sparkles: feat: Cria e estiliza componente de botão

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Botao.module.scss'
+
+const Botao = () => {
+    return (
+        <button className={styles.botao}>
+            Clique!
+        </button>
+    )
+}
+
+export default Botao
