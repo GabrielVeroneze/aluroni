8526d85d4b8ca791b8dbe2bba24706c7194e27f6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-09 21:17:55 -0300

    :fire: feat: Remove reset de estilos e estilos globais

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '@/styles/reset.css'
-import '@/styles/index.css'
 import Cardapio from '@/pages/Cardapio'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
