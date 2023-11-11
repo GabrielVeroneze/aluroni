e3e2750d8ddb2baaea79586cbd94f76bf9fe8ca3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-10 21:54:27 -0300

    :sparkles: feat: Acessa o contexto de busca
    Utiliza o contexto de busca para manipular o campo de entrada

--- src/components/CardapioSecao/Buscador/index.tsx
+++ src/components/CardapioSecao/Buscador/index.tsx
@@ -1,9 +1,17 @@
+import { useContext } from 'react'
+import { BuscaContext } from '@/context/Busca'
 import styles from './Buscador.module.scss'
 
 const Buscador = () => {
+    const { busca, setBusca } = useContext(BuscaContext)
+
     return (
         <div className={styles.container}>
-            <input className={styles.input} type="text" />
+            <input
+                className={styles.input}
+                value={busca}
+                onChange={evento => setBusca(evento.target.value)}
+            />
         </div>
     )
 }
