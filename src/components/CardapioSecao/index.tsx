17bdf25b2308bb593a30bc78a34736df0bdbfa36
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-11 16:24:59 -0300

    :sparkles: feat: Renderiza dinâmicamente os filtros

--- src/components/CardapioSecao/index.tsx
+++ src/components/CardapioSecao/index.tsx
@@ -1,4 +1,6 @@
 import Buscador from '@/components/Buscador'
+import Filtros from '@/components/Filtros'
+import filtros from '@/json/filtros.json' 
 import styles from './CardapioSecao.module.scss'
 
 const CardapioSecao = () => {
@@ -7,6 +9,9 @@ const CardapioSecao = () => {
             <h3 className={styles.cardapio__titulo}>Cardápio</h3>
             <Buscador />
             <div className={styles.cardapio__filtros}>
+                {filtros.map(opcao => (
+                    <Filtros key={opcao.id} {...opcao} />
+                ))}
             </div>
         </section>
     )
