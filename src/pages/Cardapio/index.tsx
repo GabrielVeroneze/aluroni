744f3bb3157710e22a258da1e16832292906d1fa
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-11 18:59:57 -0300

    :sparkles: feat: Fornece o contexto de filtro para toda a página

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,4 +1,5 @@
 import { BuscaProvider } from '@/context/Busca'
+import { FiltroProvider } from '@/context/Filtro'
 import Cabecalho from '@/components/Cabecalho'
 import CardapioSecao from '@/components/CardapioSecao'
 import logo from '@/assets/logo.svg'
@@ -7,6 +8,7 @@ import styles from './Cardapio.module.scss'
 const Cardapio = () => {
     return (
         <BuscaProvider>
+        <FiltroProvider>
             <main>
                 <nav className={styles.menu}>
                     <img src={logo} alt="Logo do Aluroni" />
@@ -14,6 +16,7 @@ const Cardapio = () => {
                 <Cabecalho />
                 <CardapioSecao />
             </main>
+        </FiltroProvider>
         </BuscaProvider>
     )
 }
