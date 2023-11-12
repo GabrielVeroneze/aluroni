04fe195e0ccf2255f0f099e5f349b3f2346eb3b4
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-11 23:29:18 -0300

    :sparkles: feat: Adiciona funcionalidade de abrir e fechar

--- src/components/Ordenador/index.tsx
+++ src/components/Ordenador/index.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react'
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md'
+import classNames from 'classnames'
 import Opcao from './Opcao'
 import opcoes from '@/json/opcoes.json'
 import styles from './Ordenador.module.scss'
 
 const Ordenador = () => {
+    const [aberto, setAberto] = useState<boolean>(false)
+
     return (
-        <button className={styles.ordenador}>
+        <button
+            className={styles.ordenador}
+            onClick={() => setAberto(!aberto)}
+            onBlur={() => setAberto(false)}
+        >
             <span>Ordenar Por</span>
-            <div className={styles.ordenador__options}>
+            {aberto ? (
+                <MdKeyboardArrowUp size={20} />
+            ) : (
+                <MdKeyboardArrowDown size={20} />
+            )}
+            <div
+                className={classNames({
+                    [styles.ordenador__options]: true,
+                    [styles['ordenador__options--ativo']]: aberto
+                })}
+            >
                 {opcoes.map(opcao => (
                     <Opcao key={opcao.value} {...opcao} />
                 ))}
