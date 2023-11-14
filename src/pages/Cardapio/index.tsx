b2b157fc9d3d0be6e6c5d6047e3e64269861d253
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-14 16:07:40 -0300

    :sparkles: feat: Fornece o contexto de pratos
    Fornece o contexto de pratos para poder ser utilizado em toda página

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,3 +1,4 @@
+import { PratosProvider } from '@/context/pratos'
 import { BuscaProvider } from '@/context/Busca'
 import { FiltroProvider } from '@/context/Filtro'
 import { OrdenarProvider } from '@/context/Ordenar'
@@ -8,6 +9,7 @@ import styles from './Cardapio.module.scss'
 
 const Cardapio = () => {
     return (
+        <PratosProvider>
         <BuscaProvider>
         <FiltroProvider>
         <OrdenarProvider>
@@ -21,6 +23,7 @@ const Cardapio = () => {
         </OrdenarProvider>
         </FiltroProvider>
         </BuscaProvider>
+        </PratosProvider>
     )
 }
 
