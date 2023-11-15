e75b44c304705e388e68bc1092ff5637cee538e7
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-15 18:54:03 -0300

    :sparkles: feat: Cria função utilitária para ordenar a lista

--- src/utils/controles.ts
+++ src/utils/controles.ts
@@ -1,3 +1,5 @@
+import { ICardapioItem } from '@/interfaces/ICardapioItem'
+
 export const verificaBusca = (title: string, busca: string) => {
     const regex = new RegExp(busca, 'i')
     return regex.test(title)
@@ -8,4 +10,17 @@ export const verificaFiltro = (id: number, filtro: number | null) => {
         return filtro === id
     }
     return true
+}
+
+export const ordenar = (novaLista: ICardapioItem[], ordenador: string) => {
+    switch (ordenador) {
+        case 'porcao':
+            return novaLista.sort((a, b) => a.size - b.size)
+        case 'qtd_pessoas':
+            return novaLista.sort((a, b) => a.serving - b.serving)
+        case 'preco':
+            return novaLista.sort((a, b) => a.price - b.price)
+        default:
+            return novaLista
+    }
 }
